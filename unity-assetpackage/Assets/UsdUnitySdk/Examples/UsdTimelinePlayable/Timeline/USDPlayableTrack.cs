9f57882a555e0ab98caad5bd029330696beff221
Dario Seyb <dario.seyb@rwth-aachen.de>
2018-05-14 18:47:43 +0200

    Added Unity Timeline Sample

---
+++ unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableTrack.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+namespace USD.NET.Unity.Extensions.Timeline
+{
+    [System.Serializable]
+    [TrackClipType(typeof(USDPlayableAsset))]
+    [TrackMediaType(TimelineAsset.MediaType.Script)]
+    [TrackColor(0.1f, 0.2f, 0.8f)]
+    public class USDPlayableTrack : TrackAsset
+    {
+
+    }
+}
