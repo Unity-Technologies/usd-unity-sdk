d5524eea9732cdc65b20909c9b3d52bd1cdfce35
Joshua Minor <joshm@pixar.com>
2018-05-22 10:56:40 -0700

    Conformed to coding conventions.

--- unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdBuildPostProcess.cs
+++ unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdBuildPostProcess.cs
@@ -8,17 +8,20 @@ using UnityEditor.Callbacks;
 
 public class UsdBuildPostProcess {
 
-	[PostProcessBuildAttribute(1)]
-    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
-    {
-        var source = Application.dataPath + "/UsdUnitySdk/Plugins";
-        var destination = pathToBuiltProject.Replace(".exe", "_Data/Plugins");
+  [PostProcessBuildAttribute(1)]
+  public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+  {
+    var source = Application.dataPath + "/UsdUnitySdk/Plugins";
+    var destination = pathToBuiltProject.Replace(".exe", "_Data/Plugins");
 
-        Debug.Log("USDBuildProcess copying USD plugin config files from: \"" + source + "\" to \"" + destination + "\"");
+    Debug.Log(
+      "USDBuildProcess copying USD plugin config files from: \"" +
+      source + "\" to \"" + destination + "\""
+    );
 
-        // We need to copy the whole share folder and this one plugInfo.json file
-        FileUtil.CopyFileOrDirectory(source+"/x86_64/share", destination+"/share");
-        FileUtil.CopyFileOrDirectory(source+"/x86_64/plugInfo.json", destination+"/plugInfo.json");
-    }
+    // We need to copy the whole share folder and this one plugInfo.json file
+    FileUtil.CopyFileOrDirectory(source+"/x86_64/share", destination+"/share");
+    FileUtil.CopyFileOrDirectory(source+"/x86_64/plugInfo.json", destination+"/plugInfo.json");
+  }
 }
 #endif
