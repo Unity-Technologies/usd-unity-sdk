5c5db032424293af871f2c3ff24395568fdba6fc
Dario Seyb <dario.seyb@rwth-aachen.de>
2018-05-22 10:34:49 +0200

    - Fixed whitespace

--- unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableAsset.cs
+++ unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableAsset.cs
@@ -1,37 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
 using USD.NET.Unity.Extensions.Player;
 
-namespace USD.NET.Unity.Extensions.Timeline
-{
-    [System.Serializable]
-    public class USDPlayableAsset : PlayableAsset
-    {
-        UsdPlayer player;
+namespace USD.NET.Unity.Extensions.Timeline {
+  [System.Serializable]
+  public class USDPlayableAsset : PlayableAsset {
+    UsdPlayer player;
 
-        [Tooltip("USD Player to Control")]
-        public ExposedReference<UsdPlayer> Player;
+    [Tooltip("USD Player to Control")]
+    public ExposedReference<UsdPlayer> Player;
 
-        public ClipCaps clipCaps { get { return ClipCaps.Extrapolation | ClipCaps.Looping | ClipCaps.SpeedMultiplier | ClipCaps.ClipIn; } }
+    public ClipCaps clipCaps { get { return ClipCaps.Extrapolation | ClipCaps.Looping | ClipCaps.SpeedMultiplier | ClipCaps.ClipIn; } }
 
-        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
-        {
-            var playable = ScriptPlayable<USDPlayableBehaviour>.Create(graph);
-            var behaviour = playable.GetBehaviour();
-            player = Player.Resolve(graph.GetResolver());
-            behaviour.player = player;
-            return playable;
-        }
-
-        public override double duration
-        {
-            get
-            {
-                return player == null ? 0 : player.Length;
-            }
-        }
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner) {
+      var playable = ScriptPlayable<USDPlayableBehaviour>.Create(graph);
+      var behaviour = playable.GetBehaviour();
+      player = Player.Resolve(graph.GetResolver());
+      behaviour.player = player;
+      return playable;
     }
+
+    public override double duration {
+      get {
+        return player == null ? 0 : player.Length;
+      }
+    }
+  }
 }
--- unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableTrack.cs
+++ unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableTrack.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Timeline;
 
-namespace USD.NET.Unity.Extensions.Timeline
-{
-    [System.Serializable]
-    [TrackClipType(typeof(USDPlayableAsset))]
-    [TrackMediaType(TimelineAsset.MediaType.Script)]
-    [TrackColor(0.1f, 0.2f, 0.8f)]
-    public class USDPlayableTrack : TrackAsset
-    {
+namespace USD.NET.Unity.Extensions.Timeline {
+  [System.Serializable]
+  [TrackClipType(typeof(USDPlayableAsset))]
+  [TrackMediaType(TimelineAsset.MediaType.Script)]
+  [TrackColor(0.1f, 0.2f, 0.8f)]
+  public class USDPlayableTrack : TrackAsset {
 
-    }
+  }
 }
