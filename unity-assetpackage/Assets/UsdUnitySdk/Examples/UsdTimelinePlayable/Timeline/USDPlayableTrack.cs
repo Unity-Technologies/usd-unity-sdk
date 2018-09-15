7fa019ef7ba1ae09e82f4ffd2e58a5ad95c17181
Jeremy Cowles <jeremy.cowles@gmail.com>
2018-09-15 16:40:17 -0700

    Remove deprecated Timeline attribute
    Was deprecated in Unity 2018

--- unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableTrack.cs
+++ unity-assetpackage/Assets/UsdUnitySdk/Examples/UsdTimelinePlayable/Timeline/USDPlayableTrack.cs
@@ -6,7 +6,9 @@ using UnityEngine.Timeline;
 namespace USD.NET.Unity.Extensions.Timeline {
   [System.Serializable]
   [TrackClipType(typeof(USDPlayableAsset))]
+#if UNITY_2017
   [TrackMediaType(TimelineAsset.MediaType.Script)]
+#endif
   [TrackColor(0.1f, 0.2f, 0.8f)]
   public class USDPlayableTrack : TrackAsset {
 
