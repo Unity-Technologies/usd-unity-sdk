8b868fa7051fe6dee39be23f38928501690189ae
Jeremy Cowles <jeremy.cowles@gmail.com>
2019-01-12 13:36:53 -0800

    Timeline: Remove old comment

--- unity-assetpackage/Assets/UsdUnitySdk/Scripts/Timeline/USDPlayableBehaviour.cs
+++ unity-assetpackage/Assets/UsdUnitySdk/Scripts/Timeline/USDPlayableBehaviour.cs
@@ -29,7 +29,6 @@ namespace USD.NET.Unity {
 
     // Called each frame while the state is set to Play
     public override void PrepareFrame(Playable playable, FrameData info) {
-      //player.SetTime(playable.GetTime(), player);
     }
 
     public override void ProcessFrame(Playable playable, FrameData info, object playerData) {
