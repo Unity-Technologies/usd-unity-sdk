fee92ca1d7f0ff0b651d869d0803e91de13ae8e9
Jeremy Cowles <jeremy.cowles@gmail.com>
2019-01-13 20:55:00 -0800

    Debug: Additional output to SkinnedMeshDebugger

--- unity-assetpackage/Assets/UsdUnitySdk/Scripts/Debugging/DebugSkinnedMesh.cs
+++ unity-assetpackage/Assets/UsdUnitySdk/Scripts/Debugging/DebugSkinnedMesh.cs
@@ -12,13 +12,21 @@ public class DebugSkinnedMesh : MonoBehaviour {
   [Tooltip("A prim path to the skinned mesh in the USD file")]
   public string m_usdMeshPath;
 
+  [Tooltip("How to convert transforms for comparison")]
+  public USD.NET.Unity.BasisTransformation m_basisTransform;
+
   void OnEnable() {
     USD.NET.Examples.InitUsd.Initialize();
 
     if (string.IsNullOrEmpty(m_usdMeshPath)) {
       m_usdMeshPath = USD.NET.Unity.UnityTypeConverter.GetPath(transform);
     }
-    var binding = ReadUsdWeights();
+
+    var scene = GetScene();
+    var binding = ReadUsdWeights(scene);
+    string skelRootPath;
+    var skeleton = ReadUsdSkeleton(scene, out skelRootPath);
+
     if (binding == null) {
       binding = new USD.NET.Unity.SkelBindingSample();
     }
@@ -56,15 +64,48 @@ public class DebugSkinnedMesh : MonoBehaviour {
     var bones = GetComponent<SkinnedMeshRenderer>().bones;
     var rootBone = GetComponent<SkinnedMeshRenderer>().rootBone;
     var root = USD.NET.Unity.UnityTypeConverter.GetPath(rootBone);
-
     sb.AppendLine("Bones: (" + bones.Length + ")");
     sb.AppendLine("Root Bone: " + root);
-
     int i = 0;
     foreach (var boneXf in bones) {
       sb.AppendLine(USD.NET.Unity.UnityTypeConverter.GetPath(boneXf));
       if (binding.joints != null) {
         sb.AppendLine(root + "\\" + binding.joints[i++] + "\n");
+      }
+    }
+    Debug.Log(sb.ToString());
+
+    sb = new System.Text.StringBuilder();
+    sb.AppendLine("Bind Transforms: (" + mesh.bindposes.Length + ")");
+    i = -1;
+    var options = new USD.NET.Unity.SceneImportOptions();
+    options.changeHandedness = m_basisTransform;
+    foreach (var boneXf in bones) {
+      i++;
+      var bindPose = mesh.bindposes[i];
+      var bonePath = USD.NET.Unity.UnityTypeConverter.GetPath(boneXf);
+      sb.AppendLine("Pose[" + i + "] " + bonePath);
+      sb.AppendLine(bindPose.ToString());
+
+      if (skeleton.bindTransforms != null) {
+        if (string.IsNullOrEmpty(skelRootPath)) {
+          continue;
+        }
+
+        bonePath = bonePath.Substring(skelRootPath.Length);
+        bonePath = bonePath.TrimStart('/');
+        foreach (var joint in skeleton.joints) {
+          if (joint == bonePath) {
+            var usdMat = skeleton.bindTransforms[i];
+            USD.NET.Unity.XformImporter.ImportXform(ref usdMat, options);
+            sb.AppendLine(usdMat.ToString() + "\n");
+            bonePath = null;
+            break;
+          }
+        }
+
+        if (string.IsNullOrEmpty(bonePath)) { continue; }
+        sb.Append("Bone not found in USD: " + bonePath + "\n\n");
       }
     }
     Debug.Log(sb.ToString());
@@ -101,7 +142,7 @@ public class DebugSkinnedMesh : MonoBehaviour {
     return "(" + primvar.value[usdIndex].ToString() + ")  ";
   }
 
-  USD.NET.Unity.SkelBindingSample ReadUsdWeights() {
+  USD.NET.Scene GetScene() {
     if (string.IsNullOrEmpty(m_usdFile) && string.IsNullOrEmpty(m_usdMeshPath)) {
       return null;
     }
@@ -118,10 +159,60 @@ public class DebugSkinnedMesh : MonoBehaviour {
         return null;
       }
 
+      return scene;
+    } catch (System.Exception ex) {
+      Debug.LogException(ex);
+      return null;
+    }
+  }
+
+  USD.NET.Unity.SkelBindingSample ReadUsdWeights(USD.NET.Scene scene) {
+    if (scene == null) { return null; }
+    try {
       var sample = new USD.NET.Unity.SkelBindingSample();
       scene.Read(m_usdMeshPath, sample);
       return sample;
+    } catch (System.Exception ex) {
+      Debug.LogException(ex);
+      return null;
+    }
+  }
 
+  USD.NET.Unity.SkeletonSample ReadUsdSkeleton(USD.NET.Scene scene, out string skelRootPath) {
+    skelRootPath = null;
+    if (scene == null) { return null; }
+
+    try {
+      var path = new pxr.SdfPath(m_usdMeshPath);
+      var absRoot = pxr.SdfPath.AbsoluteRootPath();
+      var skelRelName = new pxr.TfToken("skel:skeleton");
+
+      while (path != absRoot) {
+        var prim = scene.GetPrimAtPath(path);
+        path = path.GetParentPath();
+
+        if (prim.HasRelationship(skelRelName)) {
+          var targets = prim.GetRelationship(skelRelName).GetForwardedTargets();
+          if (targets.Count == 0) {
+            Debug.LogWarning("skel:skeleton has no targets at path: " + prim.GetPath());
+            continue;
+          }
+
+          var skelTarget = scene.GetPrimAtPath(targets[0]);
+          if (skelTarget == null) {
+            Debug.LogWarning("prim <" + prim.GetPath() + "> has skel:skeleton with missing target path: " + targets[0]);
+            continue;
+          }
+
+          skelRootPath = skelTarget.GetPath().ToString();
+          var sample = new USD.NET.Unity.SkeletonSample();
+          scene.Read(skelTarget.GetPath(), sample);
+          return sample;
+        }
+      }
+
+      Debug.LogWarning("Skeleton not found for path: " + m_usdMeshPath);
+      return null;
     } catch (System.Exception ex) {
       Debug.LogException(ex);
       return null;
