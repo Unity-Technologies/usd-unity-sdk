ba019f61551a45910a848d56211ebbba8324e9b4
Jeremy Cowles <jeremy.cowles@gmail.com>
2019-02-16 13:30:45 -0800

    Build: Update install scripts after package move

--- bin/install-debug.cmd
+++ bin/install-debug.cmd
@@ -9,5 +9,5 @@ copy /y "src\USD.NET.Unity\bin\x64\Debug\USD.NET.Unity.XML" "inst-dbg\"
 copy /y "src\UsdCs\x64\Debug\UsdCs.dll" "inst-dbg\"
 copy /y "src\UsdCs\x64\Debug\UsdCs.pdb" "inst-dbg\"
 
-xcopy /Y "inst-dbg\USD.NET.*" "unity-assetpackage\Assets\UsdUnitySdk\Plugins\"
-xcopy /Y "inst-dbg\UsdCs.*" "unity-assetpackage\Assets\UsdUnitySdk\Plugins\x86_64\"
+xcopy /Y "inst-dbg\USD.NET.*" "package\com.unity.formats.usd\Runtime\Plugins\"
+xcopy /Y "inst-dbg\UsdCs.*" "package\com.unity.formats.usd\Runtime\Plugins\x86_64\"
