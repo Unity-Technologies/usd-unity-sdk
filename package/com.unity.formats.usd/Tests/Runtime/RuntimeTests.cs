45e6186e9edc8dcaa4cfc0562f786398fda92397
Vlad-Andrei Lazar <vladal@unity3d.com>
2019-02-19 15:22:17 -0500

    Create empty placeholder for tests and add a dummy test

---
+++ package/com.unity.formats.usd/Tests/Runtime/RuntimeTests.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using UnityEngine.TestTools;
+
+namespace Unity.Formats.USD.Tests
+{
+    public class RuntimeTests
+    {
+        [UnityTest]
+        public IEnumerator DummyTest()
+        {
+            yield return null;
+        }
+    }
+}
