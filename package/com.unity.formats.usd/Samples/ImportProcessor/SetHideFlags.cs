67f0213f431cd7a2346aabb98aa8fd5027ceeb1a
Tim Speltz <>
2019-03-14 13:52:41 -0700

    Updated license for ImportProcessor an it's examples

--- package/com.unity.formats.usd/Samples/ImportProcessor/SetHideFlags.cs
+++ package/com.unity.formats.usd/Samples/ImportProcessor/SetHideFlags.cs
@@ -1,6 +1,4 @@
-﻿// Copyright 2019 Pixar Animation Studios
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
+﻿// Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
