ffc01efbbdbeb67f203cf8704a42e7c5d47722cd
Chris Redmann <credmann@unity3d.com>
2019-08-28 10:24:20 -0700

    replace preprocessor defines with target check

--- package/com.unity.formats.usd/Editor/Scripts/UsdBuildPostProcess.cs
+++ package/com.unity.formats.usd/Editor/Scripts/UsdBuildPostProcess.cs
@@ -21,25 +21,24 @@ namespace Unity.Formats.USD {
 
     [PostProcessBuildAttribute(1)]
     public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject) {
-#if UNITY_EDITOR_OSX
-      // plugInfo files are already in the UsdCs.bundle
-      return;
-#elif UNITY_EDITOR_LINUX
-      var source = System.IO.Path.GetFullPath("Packages/com.unity.formats.usd/Runtime/Plugins");
-      var destination = pathToBuiltProject.Replace(".x86_64", "_Data/Plugins");
 
-      // We need to copy the whole share folder and this one plugInfo.json file
-      FileUtil.CopyFileOrDirectory(source + "/x86_64/share", destination + "/share");
-      FileUtil.CopyFileOrDirectory(source + "/x86_64/plugInfo.json", destination + "/plugInfo.json");
-#else
-      var source = System.IO.Path.GetFullPath("Packages/com.unity.formats.usd/Runtime/Plugins");
-      var destination = pathToBuiltProject.Replace(".exe", "_Data/Plugins");
+        var source = System.IO.Path.GetFullPath("Packages/com.unity.formats.usd/Runtime/Plugins");
+        if (target == BuildTarget.StandaloneLinux64)
+        {
+          var destination = pathToBuiltProject.Replace(".x86_64", "_Data/Plugins");
 
-      // We need to copy the whole share folder and this one plugInfo.json file
-      FileUtil.CopyFileOrDirectory(source + "/x86_64/share", destination + "/share");
-      FileUtil.CopyFileOrDirectory(source + "/x86_64/plugInfo.json", destination + "/plugInfo.json");
-#endif
+          // We need to copy the whole share folder and this one plugInfo.json file
+          FileUtil.CopyFileOrDirectory(source + "/x86_64/share", destination + "/share");
+          FileUtil.CopyFileOrDirectory(source + "/x86_64/plugInfo.json", destination + "/plugInfo.json");
+        }
+        else if (target == BuildTarget.StandaloneWindows64)
+        {
+          var destination = pathToBuiltProject.Replace(".exe", "_Data/Plugins");
+
+          // We need to copy the whole share folder and this one plugInfo.json file
+          FileUtil.CopyFileOrDirectory(source + "/x86_64/share", destination + "/share");
+          FileUtil.CopyFileOrDirectory(source + "/x86_64/plugInfo.json", destination + "/plugInfo.json");
+        }
     }
   }
-
 }
