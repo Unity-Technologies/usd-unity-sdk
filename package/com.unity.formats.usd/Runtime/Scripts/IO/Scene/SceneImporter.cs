612ebdfab6855681327c225c4f781c12d665e885
Julien Dubuisson <julien.dubuisson@unity3d.com>
2020-03-24 17:05:30 -0400

    Remove the 2020.1 ifdef and update comment

--- package/com.unity.formats.usd/Runtime/Scripts/IO/Scene/SceneImporter.cs
+++ package/com.unity.formats.usd/Runtime/Scripts/IO/Scene/SceneImporter.cs
@@ -585,12 +585,11 @@ namespace Unity.Formats.USD {
             
             // In order to match FBX importer buggy behavior, the camera xform need an extra rotation.
             // FBX importer is fixed in 2020 though with an option to do a DeepBake
-#if !UNITY_2020_1_OR_NEWER
             if (importOptions.changeHandedness == BasisTransformation.SlowAndSafeAsFBX)
             {
-              go.transform.localRotation *= Quaternion.Euler(0.0f, -180.0f, 0.0f);
+              go.transform.localRotation *= Quaternion.Euler(180.0f, 0.0f, 180.0f); 
             }
-#endif
+
             // The camera has many value-type parameters that need to be handled correctly when not
             // not animated. For now, only the camera transform will animate, until this is fixed.
             if (scene.AccessMask == null || scene.IsPopulatingAccessMask) {
