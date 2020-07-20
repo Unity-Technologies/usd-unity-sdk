7d05e054a8f2f264345053f1a3d6e996f72b4de5
Jeremy Cowles <jeremy.cowles@gmail.com>
2020-07-19 17:55:11 -0700

    USD 20.08: Update minimal build command

--- bin/build-usd-minimal.cmd
+++ bin/build-usd-minimal.cmd
@@ -1,1 +1,1 @@
-python build_scripts\build_usd.py --build-monolithic --alembic --no-python --no-imaging ..\_build-monolithic\
+python build_scripts\build_usd.py --build-monolithic --alembic --no-python --no-imaging --no-examples --no-tutorials ..\_build-monolithic\
