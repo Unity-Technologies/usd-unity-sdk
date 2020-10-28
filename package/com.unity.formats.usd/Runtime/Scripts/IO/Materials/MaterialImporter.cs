5557aa94316ca75018c977dd43629b24ccac034a
vkovec <viktoria.kovecses@gmail.com>
2020-10-28 13:29:35 -0400

    Maintain material names on import (USDU-91 #213)
    * set material name to material path
    
    * add unit test
    
    * update changelog
    
    * use name instead of full path

--- package/com.unity.formats.usd/Runtime/Scripts/IO/Materials/MaterialImporter.cs
+++ package/com.unity.formats.usd/Runtime/Scripts/IO/Materials/MaterialImporter.cs
@@ -155,6 +155,11 @@ namespace Unity.Formats.USD {
         matAdapter.ImportFromUsd();
       }
 
+      // Get the material name from the path
+      if (mat != null && !string.IsNullOrEmpty(materialPath))
+      {
+         mat.name = new pxr.SdfPath(materialPath).GetName();
+      }
       return mat;
     }
 
