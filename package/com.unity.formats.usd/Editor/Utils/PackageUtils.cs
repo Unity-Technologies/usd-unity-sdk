46265aba178dafa86da6c759f4cd4d3bc60ee316
Marie Fetiveau <marief@unity3d.com>
2020-11-24 21:01:34 -0500

    Add relative path utilities (USDU-65)
    And tests

--- package/com.unity.formats.usd/Editor/Utils/PackageUtils.cs
+++ package/com.unity.formats.usd/Editor/Utils/PackageUtils.cs
@@ -8,6 +8,46 @@ namespace Unity.Formats.USD
 {
     public static class PackageUtils
     {
+
+        /// <summary>
+        /// Utils function to get the relative path of a file considering a base folder
+        /// </summary>
+        public static string GetRelativeFolderPath(string baseFolderPath, string filePath = "")
+        {
+            const string universalSeparator = @"/";
+            const string windowsSeparator = @"\";
+
+            // Conforming paths to ease next steps
+            filePath = filePath.Replace(windowsSeparator, universalSeparator);
+            baseFolderPath = baseFolderPath.Replace(windowsSeparator, universalSeparator);
+
+            // baseFolderPath must end with a slash to indicate folder
+            baseFolderPath = baseFolderPath.TrimEnd();
+            if (!baseFolderPath.EndsWith(universalSeparator))
+            {
+                baseFolderPath += universalSeparator;
+            }
+
+            // Using URIs to find the relative path
+            var sampleFolderAbsoluteURI = new Uri(Path.GetDirectoryName(filePath));
+            var projectFolderAbsoluteURI = new Uri(baseFolderPath);
+            var sampleFolderRelativePath =
+                Uri.UnescapeDataString(
+                    projectFolderAbsoluteURI.MakeRelativeUri(sampleFolderAbsoluteURI).ToString()
+                        .Replace('/', Path.DirectorySeparatorChar)
+                );
+            return sampleFolderRelativePath;
+        }
+
+        /// <summary>
+        /// Utils function to get the relative path of the current caller with current Unity directory as a base folder
+        /// </summary>
+        public static string GetCallerRelativeToProjectFolderPath([CallerFilePath] string filePath = "")
+        {
+            return GetRelativeFolderPath(Directory.GetCurrentDirectory(), filePath);
+        }
+
+
         /// <summary>
         /// Utils function to get the absolute path of a MonoBehaviour
         /// </summary>
