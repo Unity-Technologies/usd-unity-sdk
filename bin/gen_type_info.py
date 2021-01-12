c51d38a04bc6b06168cb7af46fe6d4cbbcc7769e
Julien Dubuisson <julien.dubuisson@unity3d.com>
2021-01-12 16:12:17 -0500

    Replace gen_type_info.cmd with a python script

---
+++ bin/gen_type_info.py
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/python3 -B
+import logging
+import subprocess
+import os
+import sys
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
+
+    # Add usd to the environment
+    usd_location = sys.argv[1]
+    new_env = os.environ.copy()
+    new_env["PATH"] = "{0}:{1}:{2}".format(os.path.join(usd_location, "bin"),
+                                           os.path.join(usd_location, "lib"),
+                                           new_env["PATH"])
+    new_env["PYTHONPATH"] = "{0}:{1}".format(os.path.join(usd_location, "lib", "python"),
+                                             new_env["PYTHONPATH"])
+    logging.getLogger().error(new_env['PATH'])
+
+    # Spawn a python process with the new environment
+    cmd = ["python3", "src\Swig\scripts\gen.py"]
+    p = subprocess.Popen(cmd, env=new_env, cwd=os.path.join(os.getcwd(), '..'))
+    p.wait()
+    if p.returncode != 0:
+        raise RuntimeError("Failed to run '{cmd}'\n.".format(cmd=cmd))
