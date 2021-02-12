d186dc8cda66973472a1b22adbb9eab687b5fd83
Julien Dubuisson <julien.dubuisson@unity3d.com>
2021-02-12 17:13:20 -0500

    Update for OSX build
    Unity only required for BUILD_USD_NET
    Update dependencies install path
    Fix VT type mapping for long long and unsigned long

--- bin/build.py
+++ bin/build.py
@@ -11,7 +11,7 @@ import zipfile
 STEVEDORE_REPO = "https://artifactory.internal.unity3d.com/stevedore-testing"
 USD_BINARIES = {"20.08": {"Windows": "usd-win-python36-x86_64/v20.08_1df762cff26f05e8c53edbc217cf0fa1210be67d75ff845316124867e33c6869.zip" ,
                           "Linux": "usd-linux-python36-x86_64/v20.08_a47ac54028df326afe4f871a1cd2b01aa3eab2b0819cc56abe1e90883d2ef97b.zip",
-                          "Darwin": "usd-mac-python36-x86_64/v20.08_e2df4db2fe24542b50f21e9ae2df45768f533c5ab03956f4d8dff64e773ed065.zip"}}
+                          "Darwin": "usd-mac-python36-x86_64/v20.08_8b0dfc648fb4dcd06938901690e9201835cb79ad51ad9eee90e7b3cfc6b3bf2c.zip"}}
 PYTHON_VERSION = "36"
 
 def usd_python_dirname(usd_version, python_version):
--- bin/gen_type_info.py
+++ bin/gen_type_info.py
@@ -15,10 +15,11 @@ if __name__ == "__main__":
     new_env = os.environ.copy()
     new_env["PATH"] = os.pathsep.join([os.path.join(usd_location, "bin"),
                                            os.path.join(usd_location, "lib"),
-                                           new_env["PATH"]])
+                                           new_env.get("PATH", "")])
+    new_env["LD_LIBRARY_PATH"] = os.pathsep.join([os.path.join(usd_location, "lib"),
+                                                  new_env.get("LD_LIBRARY_PATH", "")])
     usd_python_path = os.path.join(usd_location, "lib", "python")
-    new_env["PYTHONPATH"] = usd_python_path if "PYTHONPATH" not in new_env else \
-        os.pathsep.join([usd_python_path, new_env["PYTHONPATH"]])
+    new_env["PYTHONPATH"] = os.pathsep.join([usd_python_path, new_env.get("PYTHONPATH", "")])
 
     # Spawn a python process with the new environment
     cmd = ["python3", "src/Swig/scripts/gen.py", out_path]
