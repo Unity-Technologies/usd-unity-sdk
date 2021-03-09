b3d1e7436d7a4787329ab96f446574866308e515
Julien Dubuisson <julien.dubuisson@unity3d.com>
2021-03-09 15:06:18 -0500

    Fix hardcoded package path in PostBuildProcess and add tests

---
+++ package/com.unity.formats.usd/Tests/Editor/PostBuildTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System.IO;
+using UnityEditor;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Unity.Formats.USD.Tests
+{
+    public class PostBuildTests
+    {
+        BuildPlayerOptions m_buildOptions;
+
+        [SetUp]
+        public void SetUp()
+        {
+            m_buildOptions = new BuildPlayerOptions();
+            m_buildOptions.locationPathName = "../testBuild/dummy.exe";
+            m_buildOptions.options = BuildOptions.None;
+
+#if UNITY_EDITOR_WIN
+            m_buildOptions.target = BuildTarget.StandaloneWindows64;
+#elif UNITY_EDITOR_OSX
+            m_buildOptions.target = BuildTarget.StandaloneOSX;
+#elif UNITY_EDITOR_LINUX
+            m_buildOptions.target = BuildTarget.StandaloneLinux64;
+#endif
+
+            m_buildOptions.options = BuildOptions.None;
+
+            BuildPipeline.BuildPlayer(m_buildOptions);
+        }
+
+        [TearDown]
+        public void DeleteBuildFolder()
+        {
+            var buildFolder = new FileInfo(m_buildOptions.locationPathName).Directory;
+            Directory.Delete(buildFolder.FullName, true);
+        }
+
+
+        [Test]
+        public void PostBuild_UsdPluginsCopy()
+        {
+            var buildFolder = new FileInfo(m_buildOptions.locationPathName).Directory;
+            var dirs = buildFolder.GetDirectories("usd", SearchOption.AllDirectories);
+
+            Assert.IsNotNull(dirs, "The usd plugins directories were not found in the build.");
+        }
+    }
+}
