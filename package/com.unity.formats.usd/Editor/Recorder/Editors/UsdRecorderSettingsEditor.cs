3351bd8394b1c9a15d10566b888c01c1b714ae49
Julien Dubuisson <julien.dubuisson@unity3d.com>
2022-01-04 16:00:52 -0500

    Fix UI Labels to workaround layout issues

--- package/com.unity.formats.usd/Editor/Recorder/Editors/UsdRecorderSettingsEditor.cs
+++ package/com.unity.formats.usd/Editor/Recorder/Editors/UsdRecorderSettingsEditor.cs
@@ -1,5 +1,6 @@
 #if RECORDER_AVAILABLE
 using UnityEditor.Recorder;
+using UnityEngine;
 
 namespace UnityEditor.Formats.USD.Recorder
 {
@@ -12,8 +13,8 @@ namespace UnityEditor.Formats.USD.Recorder
             EditorGUILayout.PropertyField(serializedObject.FindProperty("interpolationType"));
             EditorGUILayout.PropertyField(serializedObject.FindProperty("coordinateConversion"));
             EditorGUILayout.PropertyField(serializedObject.FindProperty("activePolicy"));
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("exportMaterials"));
-            EditorGUILayout.PropertyField(serializedObject.FindProperty("exportTransformOverrides"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("exportMaterials"), new GUIContent("Materials"));
+            EditorGUILayout.PropertyField(serializedObject.FindProperty("exportTransformOverrides"), new GUIContent("Transform Overrides"));
             EditorGUILayout.PropertyField(serializedObject.FindProperty("scale"));
         }
     }
