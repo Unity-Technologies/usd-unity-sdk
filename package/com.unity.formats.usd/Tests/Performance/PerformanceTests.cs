9a850d13e5edcf7f61561021a0d4babd799e0b50
andrew.lee <andrew.lee@unity3d.com>
2023-02-03 16:10:21 -0500

    Adds Performance Test Project and separates Functional tests and performance tests using compilation defines

---
+++ package/com.unity.formats.usd/Tests/Performance/PerformanceTests.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NUnit.Framework;
+
+public class PerformanceTests : MonoBehaviour
+{
+    [Test]
+    public void TestTest()
+    {
+    }
+}
