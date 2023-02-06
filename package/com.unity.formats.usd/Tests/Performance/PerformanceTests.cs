4bc4f6e47d6c47e72f2d48b2271499dd4434c8c8
andrew.lee <andrew.lee@unity3d.com>
2023-02-06 10:50:38 -0500

    Adds placeholder performance test

--- package/com.unity.formats.usd/Tests/Performance/PerformanceTests.cs
+++ package/com.unity.formats.usd/Tests/Performance/PerformanceTests.cs
@@ -2,11 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using NUnit.Framework;
+using Unity.PerformanceTesting;
 
-public class PerformanceTests : MonoBehaviour
+namespace Unity.Formats.USD.Tests
 {
-    [Test]
-    public void TestTest()
+    public class PerformanceTests : BaseFixture
     {
+        [Test, Performance]
+        public void TestTest()
+        {
+            var a = Vector2.one;
+            var b = Vector2.zero;
+
+            Measure.Method(() =>
+            {
+                Vector2.MoveTowards(a, b, 0.5f);
+                Vector2.ClampMagnitude(a, 0.5f);
+                Vector2.Reflect(a, b);
+                Vector2.SignedAngle(a, b);
+            })
+                .MeasurementCount(20)
+                .Run();
+        }
     }
 }
