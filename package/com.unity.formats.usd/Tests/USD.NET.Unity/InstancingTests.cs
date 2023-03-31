8f897809a9c0d28fa967adbd500eae012eb4111e
andrew.lee <andrew.lee@unity3d.com>
2023-03-31 15:52:20 -0400

    [USDU-345] Adds Testcase for Importing Instance USD Files and their generated GameObject Axis direction

--- package/com.unity.formats.usd/Tests/USD.NET.Unity/InstancingTests.cs
+++ package/com.unity.formats.usd/Tests/USD.NET.Unity/InstancingTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Linq;
 using NUnit.Framework;
 using pxr;
+using Unity.Formats.USD;
 using UnityEngine;
 using USD.NET.Tests;
 
@@ -93,6 +94,70 @@ namespace USD.NET.Unity.Tests
                 Assert.AreEqual(Matrix4x4.Translate(new Vector3(0 + i * 2, 0, 0)), matrices[i]);
             }
             scene.Close();
+        }
+
+        [Test]
+        public void InstancerOrientation_SetToLHS()
+        {
+            const string testAssetGUID = "8ef366f1b52222347800237d47c9b0e0";
+
+            var testScene = TestUtilityFunction.OpenUSDScene(testAssetGUID);
+            var testInstanceObjectRoot = ImportHelpers.ImportSceneAsGameObject(testScene).transform.GetChild(0);
+
+            var testObjectCenter = testInstanceObjectRoot.transform.GetChild(1).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > Center
+            var testObjectLegX = testInstanceObjectRoot.transform.GetChild(1).GetChild(1).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > XPart > XLeg
+            var testObjectLegY = testInstanceObjectRoot.transform.GetChild(1).GetChild(2).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > YPart > YLeg
+            var testObjectLegZ = testInstanceObjectRoot.transform.GetChild(1).GetChild(3).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > ZPart > ZLeg
+
+            var centerCollider = testObjectCenter.gameObject.AddComponent<MeshCollider>();
+            centerCollider.sharedMesh = testObjectCenter.GetComponent<MeshFilter>().mesh;
+
+            var legXCollider = testObjectLegX.gameObject.AddComponent<MeshCollider>();
+            legXCollider.sharedMesh = testObjectLegX.GetComponent<MeshFilter>().mesh;
+
+            var legYCollider = testObjectLegY.gameObject.AddComponent<MeshCollider>();
+            legYCollider.sharedMesh = testObjectLegY.GetComponent<MeshFilter>().mesh;
+
+            var legZCollider = testObjectLegZ.gameObject.AddComponent<MeshCollider>();
+            legZCollider.sharedMesh = testObjectLegZ.GetComponent<MeshFilter>().mesh;
+
+            centerCollider.convex = true; // Physics.ComputePenetration does not work if both colliders are concave
+
+            // Control Test
+            var controlResultX = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legXCollider, testObjectLegX.position, testObjectLegX.rotation, out _, out _));
+            var controlResultY = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legYCollider, testObjectLegY.position, testObjectLegY.rotation, out _, out _));
+            var controlResultZ = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legZCollider, testObjectLegZ.position, testObjectLegZ.rotation, out _, out _));
+
+            Assert.False(controlResultX, "The X Direction Leg should not be overlapping with the center block");
+            Assert.False(controlResultY, "The Y Direction Leg should not be overlapping with the center block");
+            Assert.False(controlResultZ, "The Z Direction Leg should not be overlapping with the center block");
+
+            // Overlap Test
+            var overlapAmount = 1;
+            testObjectLegX.transform.position = new Vector3(-overlapAmount, 0, 0);
+            testObjectLegY.transform.position = new Vector3(0, -overlapAmount, 0);
+            testObjectLegZ.transform.position = new Vector3(0, 0, -overlapAmount);
+
+            var overlapResultX = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legXCollider, testObjectLegX.position, testObjectLegX.rotation, out _, out _));
+            var overlapResultY = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legYCollider, testObjectLegY.position, testObjectLegY.rotation, out _, out _));
+            var overlapResultZ = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legZCollider, testObjectLegZ.position, testObjectLegZ.rotation, out _, out _));
+
+            Assert.True(overlapResultY, $"The Y Direction Leg should be overlapping with the center block with overlap distance {overlapAmount}");
+            Assert.True(overlapResultX, $"The X Direction Leg should be overlapping with the center block with overlap distance {overlapAmount}");
+            Assert.True(overlapResultZ, $"The Z Direction Leg should be overlapping with the center block with overlap distance {overlapAmount}");
+
+            // Distanced Test
+            testObjectLegX.transform.position = new Vector3(1, 0, 0);
+            testObjectLegY.transform.position = new Vector3(0, 1, 0);
+            testObjectLegZ.transform.position = new Vector3(0, 0, 1);
+
+            var distancedResultX = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legXCollider, testObjectLegX.position, testObjectLegX.rotation, out _, out _));
+            var distancedResultY = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legYCollider, testObjectLegY.position, testObjectLegY.rotation, out _, out _));
+            var distancedResultZ = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legZCollider, testObjectLegZ.position, testObjectLegZ.rotation, out _, out _));
+
+            Assert.False(distancedResultX, "The X Direction Leg should not be overlapping with the center block");
+            Assert.False(distancedResultY, "The Y Direction Leg should not be overlapping with the center block");
+            Assert.False(distancedResultZ, "The Z Direction Leg should not be overlapping with the center block");
         }
     }
 }
