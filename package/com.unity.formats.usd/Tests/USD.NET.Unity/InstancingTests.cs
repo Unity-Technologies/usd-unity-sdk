6ed554d5e5d8851403f1ce1198b752f64a1824bf
andrew.lee <andrew.lee@unity3d.com>
2023-04-03 13:50:46 -0400

    Replaces long physics based testcase with simpler vertex position check test

--- package/com.unity.formats.usd/Tests/USD.NET.Unity/InstancingTests.cs
+++ package/com.unity.formats.usd/Tests/USD.NET.Unity/InstancingTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
@@ -100,69 +101,20 @@ namespace USD.NET.Unity.Tests
 
         [Test]
         [Ignore("TODO: Enable this when fix for this has been applied")]
-        public void InstancerImport_LeftHandedAxis_IsApplied()
+        public void InstancerImport_LeftHandedAxis_VertexCheck()
         {
-            const string testAssetGUID = "8ef366f1b52222347800237d47c9b0e0";
+            HashSet<Vector3> expectedVertexPoints = new HashSet<Vector3> { new Vector3(-1, 0, 3), new Vector3(-1, 0, 0), new Vector3(0, -1, 1), new Vector3(-2, -3, -1), new Vector3(-2, -1, 1), new Vector3(-3, -0.5f, -1), new Vector3(-1, -2, 1), new Vector3(-5, -2, -1) };
+            const string testAssetGUID = "e39567e914fdf7240b456e7519d0deba";
 
             var testScenePath = AssetDatabase.GUIDToAssetPath(testAssetGUID);
             var testScene = ImportHelpers.InitForOpen(Path.GetFullPath(testScenePath));
 
-            var testInstanceObjectRoot = ImportHelpers.ImportSceneAsGameObject(testScene).transform.GetChild(0);
+            var testInstanceObjectMesh = ImportHelpers.ImportSceneAsGameObject(testScene).GetComponentInChildren<MeshFilter>().mesh;
 
-            var testObjectCenter = testInstanceObjectRoot.transform.GetChild(1).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > Center
-            var testObjectLegX = testInstanceObjectRoot.transform.GetChild(1).GetChild(1).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > XPart > XLeg
-            var testObjectLegY = testInstanceObjectRoot.transform.GetChild(1).GetChild(2).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > YPart > YLeg
-            var testObjectLegZ = testInstanceObjectRoot.transform.GetChild(1).GetChild(3).GetChild(0); // axisTest_instanced > instancers > axisTest_0 > ZPart > ZLeg
-
-            var centerCollider = testObjectCenter.gameObject.AddComponent<MeshCollider>();
-            centerCollider.sharedMesh = testObjectCenter.GetComponent<MeshFilter>().mesh;
-
-            var legXCollider = testObjectLegX.gameObject.AddComponent<MeshCollider>();
-            legXCollider.sharedMesh = testObjectLegX.GetComponent<MeshFilter>().mesh;
-
-            var legYCollider = testObjectLegY.gameObject.AddComponent<MeshCollider>();
-            legYCollider.sharedMesh = testObjectLegY.GetComponent<MeshFilter>().mesh;
-
-            var legZCollider = testObjectLegZ.gameObject.AddComponent<MeshCollider>();
-            legZCollider.sharedMesh = testObjectLegZ.GetComponent<MeshFilter>().mesh;
-
-            centerCollider.convex = true; // Physics.ComputePenetration does not work if both colliders are concave
-
-            // Control Test
-            var controlResultX = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legXCollider, testObjectLegX.position, testObjectLegX.rotation, out _, out _));
-            var controlResultY = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legYCollider, testObjectLegY.position, testObjectLegY.rotation, out _, out _));
-            var controlResultZ = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legZCollider, testObjectLegZ.position, testObjectLegZ.rotation, out _, out _));
-
-            Assert.False(controlResultX, "The X Direction Leg should not be overlapping with the center block");
-            Assert.False(controlResultY, "The Y Direction Leg should not be overlapping with the center block");
-            Assert.False(controlResultZ, "The Z Direction Leg should not be overlapping with the center block");
-
-            // Overlap Test
-            var overlapAmount = 1;
-            testObjectLegX.transform.position = new Vector3(-overlapAmount, 0, 0);
-            testObjectLegY.transform.position = new Vector3(0, -overlapAmount, 0);
-            testObjectLegZ.transform.position = new Vector3(0, 0, -overlapAmount);
-
-            var overlapResultX = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legXCollider, testObjectLegX.position, testObjectLegX.rotation, out _, out _));
-            var overlapResultY = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legYCollider, testObjectLegY.position, testObjectLegY.rotation, out _, out _));
-            var overlapResultZ = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legZCollider, testObjectLegZ.position, testObjectLegZ.rotation, out _, out _));
-
-            Assert.True(overlapResultY, $"The Y Direction Leg should be overlapping with the center block with overlap distance {overlapAmount}");
-            Assert.True(overlapResultX, $"The X Direction Leg should be overlapping with the center block with overlap distance {overlapAmount}");
-            Assert.True(overlapResultZ, $"The Z Direction Leg should be overlapping with the center block with overlap distance {overlapAmount}");
-
-            // Distanced Test
-            testObjectLegX.transform.position = new Vector3(1, 0, 0);
-            testObjectLegY.transform.position = new Vector3(0, 1, 0);
-            testObjectLegZ.transform.position = new Vector3(0, 0, 1);
-
-            var distancedResultX = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legXCollider, testObjectLegX.position, testObjectLegX.rotation, out _, out _));
-            var distancedResultY = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legYCollider, testObjectLegY.position, testObjectLegY.rotation, out _, out _));
-            var distancedResultZ = (Physics.ComputePenetration(centerCollider, testObjectCenter.position, testObjectCenter.rotation, legZCollider, testObjectLegZ.position, testObjectLegZ.rotation, out _, out _));
-
-            Assert.False(distancedResultX, "The X Direction Leg should not be overlapping with the center block");
-            Assert.False(distancedResultY, "The Y Direction Leg should not be overlapping with the center block");
-            Assert.False(distancedResultZ, "The Z Direction Leg should not be overlapping with the center block");
+            foreach (var vertex in testInstanceObjectMesh.vertices)
+            {
+                Assert.That(expectedVertexPoints.Contains(vertex), $"Vertex <{vertex}> is not found in \n\t{string.Join("\n\t", expectedVertexPoints)}");
+            }
         }
     }
 }
